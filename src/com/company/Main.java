820b423a2738e6f7ff53e36002b11909c9888eb5
Xavier O <xavier.odonovan.2021@student.lhschools.org>
2020-03-05 13:52:47 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// todo even or odd
+    // todo keep number contained
+    // todo get the last digit
+
+    int number = 5;
+
+
+
+    }
+}
