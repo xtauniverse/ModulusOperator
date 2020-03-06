c27c61c860f3de3a15aeebd2fc26802f8e80f6bc
Xavier O <xavier.odonovan.2021@student.lhschools.org>
2020-03-06 13:33:12 -0500

    OddnessMethodCreated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// todo even or odd
-    // todo keep number contained
-    // todo get the last digit
 
-    int number = 5;
+        testOddness();
+
+        // todo even or odd
+        // todo keep number contained
+        // todo get the last digit
+    }
+
+    public static void testOddness(){
+
+        Scanner keyboard;
+
+        int numberPositive;
+
+        keyboard = new Scanner(System.in);
+
+
+
+    System.out.println("Enter a positive number");
+    numberPositive = keyboard.nextInt();
+
+
+        System.out.println("Number is odd: " + (numberPositive % 2 !=0));
 
 
 
