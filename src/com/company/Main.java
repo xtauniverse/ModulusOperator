edf0c5a751873c4407386bf7b493c32e3f3384c3
Xavier O <xavier.odonovan.2021@student.lhschools.org>
2020-03-12 13:41:23 -0400

    lastDigitMethodCreated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         testOddness();
         keepContained();
-
+        lastDigit();
 
         // todo even or odd
         // todo keep number contained
@@ -46,4 +46,20 @@ public class Main {
 
 
     }
+
+    public static void lastDigit(){
+
+        Scanner keyboard;
+
+        int numberPositiveFourDigits;
+
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Enter a positive number that is 4 digits long");
+        numberPositiveFourDigits = keyboard.nextInt();
+
+        System.out.println("The last digit of the number is: " + numberPositiveFourDigits%10);
+
+    }
+
 }
